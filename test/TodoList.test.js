bd2f4cd8fb7dcc4f457d3ee3211b081bfabf128f
chriszer tamayo <chriszer.myth@gmail.com>
2019-08-31 17:05:57 +0800

    3. Creates Tasks

--- test/TodoList.test.js
+++ test/TodoList.test.js
@@ -20,7 +20,15 @@ contract('TodoLost',(accounts) => {
        assert.equal(task.content, 'Check out dappuniversity.com')
        assert.equal(task.completed,false)
        assert.equal(taskCount.toNumber(),1)
-       
+   })
 
+   it('creates tasks', async () => {
+     const result = await this.todoList.createTask('A new task')
+     const taskCount = await this.todoList.taskCount()
+     assert.equal(taskCount, 2)
+     const event = result.logs[0].args
+     assert.equal(event.id.toNumber(),2)
+     assert.equal(event.content,'A new task')
+     assert.equal(event.completed,false)
    })
 })
