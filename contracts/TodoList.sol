f218c6ff6ec5a67035bf5f5979878e0895d4ec0b
chriszer tamayo <chriszer.myth@gmail.com>
2019-08-31 17:35:10 +0800

    Complete Task

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -16,6 +16,11 @@ contract TodoList {
      string content,
      bool completed
    );
+   
+   event TaskCompleted(
+     uint id,
+     bool completed
+   );
 
    constructor() public {
      createTask("Check out dappuniversity.com");
@@ -27,4 +32,10 @@ contract TodoList {
       emit TaskCreated(taskCount, _content, false);
    }
 
+   function toggleCompleted(uint _id) public {
+    Task memory _task = tasks[_id];
+    _task.completed = !_task.completed;
+    tasks[_id] = _task;
+    emit TaskCompleted(_id,_task.completed); 
+   }
 }
